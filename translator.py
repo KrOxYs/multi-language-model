f07b976cc609d7b86e696755301f81ca2af85770
KrOxYs <raifandibaiqi@gmail.com>
2024-12-19 23:43:46 +0700

    fix bug

--- translator.py
+++ translator.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     tokenizer = M2M100Tokenizer.from_pretrained('facebook/m2m100_418M')
 
     # Input parameters
-    text = "Hello nama saya raifandi saya tinggal di indonesia lebih tepatnya di jakarta"
+    text = "The rapid progress of artificial intelligence (IA) has transformed sectors around the world. From healthcare to finance, AI technologies are used to optimize processes, improve decision-making and offer innovative solutions. One of the most notable advances of AI is the natural language processing (PLN), which allows machines to understand, interpret and generate human language. This has led to the development of applications such as chatbots, language translation tools and automatic content generation. For example, in healthcare, AI-based systems help to perform early diagnoses by analysing medical data and images. In the automotive industry, automotive cars are becoming a real-world processing algorithm."
     source_lang = 'en'
-    target_lang = 'id'
+    target_lang = 'es'
 
     # Set source language
     tokenizer.src_lang = source_lang
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     
     # Decode and print translation
     translation = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
-    print(f"{source_lang.upper()} → {target_lang.upper()}: {text} → {translation[0]}")
+    print(f"{source_lang.upper()} → {target_lang.upper()}: → {translation[0]}")
 
     # End timer
     end_time = time.time()
